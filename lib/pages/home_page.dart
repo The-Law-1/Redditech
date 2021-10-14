f6c254b8a04cf2d40d1e468ad70a96a6882aba72
Gabriel <gabriel.griffin@epitech.eu>
2021-10-14 19:46:32 +0200

    Added disconnected dropdown and home feed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,26 +4,61 @@ import '../main.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import '../widgets/feed.dart';
 
-class HomePage extends StatelessWidget /* with RedditInfo*/ {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> /* with RedditInfo*/ {
+  List<Widget> postsFeed = createPostsFeed("Hot");
+  String dropdownValue = "Hot";
+
+  List<Widget> homeElements = [];
 
   @override
   Widget build(BuildContext context) {
+    homeElements.add(filtersDropDown());
+    homeElements.addAll(postsFeed);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Home'),
-      ),
-      body: Center(
-        // tabs for HOT/BEST/NEW
-        // list view of posts
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: createPostsFeed("Hot")
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text('Home'),
         ),
-
-      )
-    );
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: homeElements,
+          // tabs for HOT/BEST/NEW
+          ),
+          //child: ListView(padding: EdgeInsets.zero, children: postsFeed),
+          // list view of posts
+          // ),
+        );
   }
+
+  Widget filtersDropDown() => DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+          postsFeed = createPostsFeed(dropdownValue);
+        });
+      },
+      items: <String>['Hot', 'Best', 'New', 'Popular']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList());
 }
