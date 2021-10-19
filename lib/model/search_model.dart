0842bd940fddc436db91246f86696a4d4d069ae2
Nywes <Nywes@localhost.localdomain>
2021-10-19 17:43:35 +0200

    better version of the search bar

--- lib/model/search_model.dart
+++ lib/model/search_model.dart
@@ -1,31 +1,54 @@
 import 'dart:convert';
-import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:redditech/controller/search_controller.dart';
 import '../widgets/feed.dart';
 
-class Subreddit {
+class SubredditModel {
   late String subredditName = "";
   late String subredditImgUrl = "";
   late String textContent = "";
+
+  SubredditModel(this.subredditName, this.subredditImgUrl, this.textContent);
 }
 
 class SubredditFeed {
-  late List<Subreddit> subreddits = [];
+  late List<SubredditModel> subreddits = [];
 
   Future<bool> setInfo(String query) async {
     String postsList = await SearchController.GetSubreddits(query);
+    String iconImg = "";
 
+    print("QUERY: " + query);
+    if (postsList == "") {
+      return false;
+    }
     var jsonPosts = jsonDecode(postsList);
-    var data = jsonPosts["data"];
-    List actualPosts = data["children"];
-    var firstPost = actualPosts[0]["data"];
-    var secPost = actualPosts[1]["data"];
-    var thirdPost = actualPosts[2]["data"];
+    print("JSON: " + jsonPosts.toString());
+    var data = jsonPosts['data'];
+    List actualPosts = data['children'];
+    //var firstPost = actualPosts[0]["data"];
 
-    print("POST 1 : " + firstPost['title']);
-    print("POST 2 : " + secPost['title']);
-    print("POST 3 : " + thirdPost['title']);
+    for (var i = 0; i < actualPosts.length; i++) {
+      var postData = actualPosts[i]['data'];
+
+      if (postData['icon_img'] == null) {
+        iconImg = "";
+      }
+      SubredditModel newSubreddit = SubredditModel(
+          postData['title'], iconImg, postData['public_description']);
+      subreddits.add(newSubreddit);
+    }
+    //print("POST 1 : " + firstPost['title']);
     return (true);
+  }
+
+  List<Widget> getFeed() {
+    List<Widget> postWidgets = [];
+
+    for (var i = 0; i < subreddits.length; i++) {
+      postWidgets.add(subredditRow(
+          subreddits[i].subredditImgUrl, subreddits[i].subredditName));
+    }
+    return (postWidgets);
   }
 }
--- lib/pages/searching_page.dart
+++ lib/pages/searching_page.dart
@@ -11,70 +11,101 @@ class SearchingPage extends StatefulWidget {
 
 class _SearchingPageState extends State<SearchingPage> {
   SubredditFeed subredditFeed = SubredditFeed();
+  List<Widget> subredditsFeed = [];
+  bool initialized = false;
+
+  void Initialize(BuildContext context) async {
+    if (initialized) {
+      return;
+    }
+    await subredditFeed.setInfo("reddit");
+    setState(() {
+      subredditsFeed = [];
+      subredditsFeed = subredditFeed.getFeed();
+      initialized = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Initialize(context);
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          buildFloatingSearchBar(context, subredditFeed),
-        ],
-      ),
+      body: Stack(fit: StackFit.expand, children: [
+        ListView(
+          children: subredditsFeed,
+        ),
+        buildFloatingSearchBar(context),
+      ]),
     );
   }
-}
 
-Widget buildFloatingSearchBar(
-    BuildContext context, SubredditFeed subredditFeed) {
-  final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+  Widget buildFloatingSearchBar(BuildContext context) {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    String globalQuery = "";
 
-  return FloatingSearchBar(
-    hint: 'Search...',
-    scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-    transitionDuration: const Duration(milliseconds: 800),
-    transitionCurve: Curves.easeInOut,
-    physics: const BouncingScrollPhysics(),
-    axisAlignment: isPortrait ? 0.0 : -1.0,
-    openAxisAlignment: 0.0,
-    width: isPortrait ? 600 : 500,
-    debounceDelay: const Duration(milliseconds: 500),
-    onQueryChanged: (query) {
-      //ici j'appelle l'api avec query qui est le mot en param
-      subredditFeed.setInfo(query);
-      // Call your model, bloc, controller here.
-    },
-    // Specify a custom transition to be used for
-    // animating between opened and closed stated.
-    transition: CircularFloatingSearchBarTransition(),
-    actions: [
-      FloatingSearchBarAction(
-        showIfOpened: false,
-        child: CircularButton(
-          icon: const Icon(Icons.manage_search),
-          onPressed: () {
-            //search
-          },
-        ),
-      ),
-      FloatingSearchBarAction.searchToClear(
-        showIfClosed: false,
-      ),
-    ],
-    builder: (context, transition) {
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Material(
-          color: Colors.white,
-          elevation: 4.0,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: Colors.accents.map((color) {
-              return Container(height: 112, color: color);
-            }).toList(),
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onSubmitted: (query) {
+        print("On submitted " + query);
+        setState(() async {
+          await subredditFeed.setInfo(query);
+          subredditsFeed = [];
+          subredditsFeed = subredditFeed.getFeed();
+        });
+      },
+      onQueryChanged: (query) {
+        //ici j'appelle l'api avec query qui est le mot en param
+        //subredditFeed.setInfo(query);
+        setState(() {
+          globalQuery = query;
+        });
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.manage_search),
+            onPressed: () {
+              print("On pressed");
+              setState(() async {
+                await subredditFeed.setInfo(globalQuery);
+                subredditsFeed = [];
+                subredditsFeed = subredditFeed.getFeed();
+              });
+              //search
+            },
           ),
         ),
-      );
-    },
-  );
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
